0deb5c2590fbd1003c73e047b5432f5046acd049
Erik Fox <erfox@auto.ece.pdx.edu>
2021-03-01 12:18:05 -0800

    compiles, ready for testing

--- simplebusif.sv
+++ simplebusif.sv
@@ -43,26 +43,27 @@ endmodule
 
 module ProcessorIntThread(simplebus bus);
 
-logic en_AddrUp, en_AddrLo, ld_Data, en_Data, access = 0;
+logic en_AddrUp, en_AddrMid, en_AddrLo, ld_Data, en_Data, access = 0;
 logic doRead, wDataRdy, dv;
 logic [7:0] DataReg;
-logic [15:0] AddrReg;
+logic [23:0] AddrReg;
 
-enum {MA,MB,MC,MD} State, NextState;
+enum {ONE,TWO,THREE,FOUR,FIVE} State, NextState;
 
 assign bus.data = (en_Data) ? DataReg : 'bz;
-assign bus.dataValid = (State == MD) ? dv : 1'bz;
+assign bus.dataValid = (State == FIVE) ? dv : 1'bz;
 
 always_comb
     if (en_AddrLo) bus.address = AddrReg[7:0];
-    else if (en_AddrUp) bus.address = AddrReg[15:8];
+    else if (en_AddrMid) bus.address = AddrReg[15:8];
+    else if (en_AddrUp) bus.address = AddrReg[23:16];
     else bus.address = 'bz;
     
 always_ff @(posedge bus.clock)
     if (ld_Data) DataReg <= bus.data;
     
 always_ff @(posedge bus.clock, negedge bus.resetN)
-    if (!bus.resetN) State <= MA;
+    if (!bus.resetN) State <= ONE;
     else State <= NextState;
     
     
@@ -70,6 +71,7 @@ always_comb
     begin
     bus.start = 0;
     en_AddrUp = 0;
+    en_AddrMid = 0;
     en_AddrLo = 0;
     bus.read = 0;
     ld_Data = 0;
@@ -77,29 +79,34 @@ always_comb
     dv = 0;
     
     case(State)
-    MA:	begin
-    	NextState = (access) ? MB : MA;
+    ONE:begin
+    	
+	NextState = (access) ? TWO : ONE;
     	bus.start = (access) ? 1 : 0;
     	en_AddrUp = (access) ? 1 : 0;
     	end
-    MB:	begin
-    	NextState = (doRead) ? MC : MD;
+    TWO:begin
+	NextState=THREE;
+    	en_AddrMid = 1 ;
+	end
+    THREE:begin
+    	NextState = (doRead) ? FOUR : FIVE ;
     	en_AddrLo = 1;
     	bus.read = (doRead) ? 1 : 0;
     	end
-    MC:	begin
-    	NextState = (bus.dataValid) ? MA : MC;
+    FOUR:begin
+    	NextState = (bus.dataValid) ? ONE : FOUR;
     	ld_Data = (bus.dataValid) ? 1 : 0;
     	end
-    MD:	begin
-    	NextState = (wDataRdy) ? MA : MD;
+    FIVE:begin
+    	NextState = (wDataRdy) ? ONE: FIVE;
     	en_Data = (wDataRdy) ? 1 : 0;
     	dv = (wDataRdy) ? 1 : 0;
     	end
     endcase
     end
 
-task WriteMem(input [15:0] Avalue, input [7:0] Dvalue);   
+task WriteMem(input [23:0] Avalue, input [7:0] Dvalue);   
 begin
 access <= 1;
 doRead <= 0;
@@ -108,13 +115,13 @@ AddrReg <= Avalue;
 DataReg <= Dvalue;
 @(posedge bus.clock) access <= 0;
 @(posedge bus.clock);
-wait (State == MA); 
+wait (State == ONE); 
 repeat (2) @(posedge bus.clock);
 end
 endtask
 
 
-task ReadMem(input [15:0] Avalue);   
+task ReadMem(input [23:0] Avalue);   
 begin
 access <= 1;
 doRead <= 1;
@@ -122,7 +129,7 @@ wDataRdy <= 0;
 AddrReg <= Avalue;
   @(posedge bus.clock) access <= 0;
   @(posedge bus.clock);
-wait (State == MA); 
+wait (State == ONE); 
   repeat (2) @(posedge bus.clock);
 end
 endtask
@@ -132,12 +139,12 @@ initial
 begin
 repeat (2) @(posedge bus.clock);
 // Note this is from the textbook but is *not* a good test!!
-WriteMem(16'h0406, 8'hDC);
-WriteMem(16'h0407, 8'hAB);
-ReadMem(16'h0406);
-ReadMem(16'h0407);
-WriteMem(16'h0406, 8'hF1);
-ReadMem(16'h0406);
+WriteMem(24'h010406, 8'hDC);
+WriteMem(24'h010407, 8'hAB);
+ReadMem(24'h010406);
+ReadMem(24'h010407);
+WriteMem(24'h010406, 8'hF1);
+ReadMem(24'h010406);
 $finish;
 end
     
@@ -148,15 +155,15 @@ endmodule
 
 
 
-module MemoryIntThread(simplebus bus);
+module MemoryIntThread#(parameter N=0)(simplebus bus);
     
 logic [7:0] Mem[16'hFFFF:0], MemData;
-logic ld_AddrUp, ld_AddrLo, memDataAvail = 0;
+logic ld_AddrUp, ld_AddrLo,ld_AddrMid, memDataAvail = 0;
 logic en_Data, ld_Data, dv;
 logic [7:0] DataReg;
-logic [15:0] AddrReg;
+logic [23:0] AddrReg;
 
-enum {SA, SB, SC, SD} State, NextState;
+enum {ONE, TWO, THREE, FOUR,FIVE} State, NextState;
 
 
 initial
@@ -167,14 +174,16 @@ initial
 
     
 assign bus.data = (en_Data) ? MemData : 'bz;
-assign bus.dataValid = (State == SC) ? dv : 1'bz;
+assign bus.dataValid = (State == FOUR) ? dv : 1'bz;
 
 
 always @(AddrReg, ld_Data)
     MemData = Mem[AddrReg];
     
 always_ff @(posedge bus.clock)
-    if (ld_AddrUp) AddrReg[15:8] <= bus.address;
+    if (ld_AddrUp) AddrReg[23:16] <= bus.address;
+always_ff @(posedge bus.clock)
+    if (ld_AddrMid) AddrReg[15:8] <= bus.address;
     
 always_ff @(posedge bus.clock)
   if (ld_AddrLo) AddrReg[7:0] <= bus.address;
@@ -189,33 +198,38 @@ always @(posedge bus.clock)
     end
     
 always_ff @(posedge bus.clock, negedge bus.resetN)
-  if (!bus.resetN) State <= SA;
+  if (!bus.resetN) State <= ONE;
   else State <= NextState;
   
 always_comb
     begin
     ld_AddrUp = 0;
+    ld_AddrMid = 0;
     ld_AddrLo = 0;
     dv = 0;
     en_Data = 0;
     ld_Data = 0;
     
     case (State)
-    SA: begin
-      NextState = (bus.start) ? SB : SA;
-      ld_AddrUp = (bus.start) ? 1 : 0;
+    ONE: begin
+        NextState = (bus.start) ? TWO : ONE;
+        ld_AddrUp = (bus.start) ? 1 : 0;
     	end
-    SB: begin
-      NextState = (bus.read) ? SC : SD;
+    TWO:begin
+	NextState=(AddrReg[23:16]==N)?THREE:ONE;
+	ld_AddrMid=1;
+	end
+    THREE: begin
+	NextState = (bus.read) ? FOUR : FIVE;
     	ld_AddrLo = 1;
     	end
-    SC: begin
-    	NextState = (memDataAvail) ? SA : SC;
+    FOUR: begin
+    	NextState = (memDataAvail) ? ONE : FOUR;
     	dv = (memDataAvail) ? 1 : 0;
     	en_Data = (memDataAvail) ? 1 : 0;
     	end
-    SD: begin
-      NextState = (bus.dataValid) ? SA: SD;
+    FIVE: begin
+      NextState = (bus.dataValid) ? ONE: FIVE;
       ld_Data = (bus.dataValid) ? 1 : 0;
     	end
     endcase
@@ -226,7 +240,7 @@ always_comb
     begin
     bit [2:0] delay;
     memDataAvail <= 0;
-    if (State == SC)
+    if (State == FOUR)
     	begin
     	delay = $random;
     	repeat (2 + delay)
